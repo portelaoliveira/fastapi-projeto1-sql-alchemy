7c7aa99ef69b4c411e86e03b22da526801452e81
portelaoliveira <daniloportela97@gmail.com>
2022-11-15 17:14:55 -0300

    Trabalhando no módulo api parte 1.

--- models/curso_model.py
+++ models/curso_model.py
@@ -2,7 +2,7 @@ from core.configs import settings
 
 from sqlalchemy import Column, Integer, String
 
-class CusoModel(settings.DBBaseModel):
+class CursoModel(settings.DBBaseModel):
     __tablename__ = 'cursos'
 
     id: int = Column(Integer, primary_key=True, autoincrement=True)
